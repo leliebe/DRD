edc763dc7d13c708c766103728171f59a7bc7b80
leliebe <leliebe@users.noreply.github.com>
2019-09-18 19:41:38 -0500

    Added Resource & Parser Bones

--- DRD_Resource.php
+++ DRD_Resource.php
@@ -1,7 +1,19 @@
 <?php include('head.php'); ?>
 
-<div class="content">
+<div class="resource-content">
     <!-- Code for page goes here  -->
-    <p> This is the Data page. </p>
+    <p> This page includes links to other projects that may be of interest to those studying Restoration drama. </p>
+    <div class="resource-list">
+      <ul style="list-style-type:none;">
+        <li>
+          <p class="resource-link"><a href="http://institucional.us.es/restoration/catalogue.html">The Restoration Comedy Project</a></p>
+          <p class="resource-desc">A database of comedies produced from the Restoration through the first decades of the eighteenth century.
+            Includes performance and publication data as well as selected links to full texts and detailed summaries.</p>
+        </li>
+        <li>
+          <p class="resource-link"><a href="https://restorationstaging.com/">Restoration Staging</a></p>
+          <p class="resource-desc">A blog-style project including research articles and resources for the study of restoration staging and performance practices.</p>
+      </ul>
+    </div>
 </div>
 <?php include('footer.php'); ?>
