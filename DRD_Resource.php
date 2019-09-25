fd008af9344ff29be4f8ccd205c5013f4a09b938
leliebe <leliebe@users.noreply.github.com>
2019-09-24 23:35:00 -0500

    9/24 Update, CSS Complete
    XML still needs editing for <seg> around names in the play body and to remove all <hi rend="italic"> tags throughout.

--- DRD_Resource.php
+++ DRD_Resource.php
@@ -2,7 +2,7 @@
 
 <div class="resource-content">
     <!-- Code for page goes here  -->
-    <p> This page includes links to other projects that may be of interest to those studying Restoration drama. </p>
+    <p> This page includes links to other freely available projects that may be of interest to those studying Restoration drama. </p>
     <div class="resource-list">
       <ul style="list-style-type:none;">
         <li>
@@ -13,6 +13,15 @@
         <li>
           <p class="resource-link"><a href="https://restorationstaging.com/">Restoration Staging</a></p>
           <p class="resource-desc">A blog-style project including research articles and resources for the study of restoration staging and performance practices.</p>
+        </li>
+        <li>
+          <p class="resource-link"><a href="https://www.pepysdiary.com/">The Diary of Samuel Pepys</a></p>
+          <p class="resource-desc">Digital version of Pepys's diary, including wiki-style links and annotations.</p>
+        </li>
+        <li>
+          <p class="resource-link"><a href="http://bsuva.org/bsuva/promptbook/">Shakespearean Prompt-Books of the Seventeenth Century</a></p>
+          <p class="resource-desc">Descriptions of annotations in seventeenth-century prompt-books alongside brief explanatory essays.</p>
+        </li>
       </ul>
     </div>
 </div>
--- xml_parser.php
+++ xml_parser.php
@@ -6,49 +6,387 @@ function parse_xml_file($file_name)
     // include('./database/db_query.php');
     // $play_title = $_POST['play_title'];
 
-    $xml = simplexml_load_file($file_name);
+    $xml = simplexml_load_file($file_name) or die("Error: Cannot create object");
+
     // $title = $xml->teiHeader->fileDesc->titleStmt->title;
     // $author1 = $xml->teiHeader->fileDesc->titleStmt->author[0];
     // $author2 = $xml->teiHeader->fileDesc->titleStmt->author[1];
     // echo  "<h1>$title</h1></br>";
-    $clvls = "->text->front->titlePage->";
+    $count = 1;
+    $wline = "";
+    echo "<div class='titlepart'>";
+    foreach ($xml->text->front->titlePage->docTitle->titlePart as $titlepart)
+    {
+        // $count++;
+        foreach($titlepart->line as $tpline)
+        {
+            echo "<p class='tp$count'>$tpline</p>";
+            foreach($tpline->seg as $tpseg)
+            {
+                $wline .= $tpseg;
+                if ($tpseg->persName->hi)
+                {
+                    $wline .= $tpseg->persName->hi;
+                }
+            }
+            echo "<p class='tp$count'>$wline</p>";
+            $count++;
+        }
+        echo "<p class='tp$count'>$titlepart</p>";
+        $count++;
+    }
+    echo "</div>";//close titlepart
 
-    $titlepage1 = $xml->text->front->titlePage->docTitle->titlePart[0];
-    $titlepage2 = $xml->text->front->titlePage->docTitle->titlePart[1];
-    $titlepage3 = $xml->text->front->titlePage->docTitle->titlePart[1]->seg;
-    $titlepage4 = $xml->text->front->titlePage->docTitle->titlePart[2];
-    $titlepage5 = $xml->text->front->titlePage->docTitle->titlePart[2]->seg[0];
-    $titlepage6 = $xml->text->front->titlePage->docTitle->titlePart[2]->seg[1];
-    $titlepage7 = $xml->text->front->titlePage->docTitle->titlePart[2]->seg[2];
-    $titlepage8 = $xml->text->front->titlePage->docTitle->titlePart[2]->seg[2]->persName->hi;
-    $titlepage9 = $xml->text->front->titlePage->docTitle->titlePart[2]->seg[3];
+
+    echo "<div class='byline'>";
     $byline = $xml->text->front->titlePage->byline . $xml->text->front->titlePage->byline->docAuthor->persName->hi;
-    $imp1 =   $xml->text->front->titlePage->imprimatur->seg[0];
-    $imp2 =   $xml->text->front->titlePage->imprimatur->seg[1];
-    $imp3 =   $xml->text->front->titlePage->imprimatur->seg[2];
-    $imp4 =   $xml->text->front->titlePage->imprimatur->seg[3];
-    // $titlepage9 = $xml->text->front->titlePage->docTitle->titlePart[2]->seg[2]->seg;
-    echo "<h1 class='tp1'>$titlepage1</h1>";
-    echo "<h2 class='tp2'>$titlepage2</h2>";
-    echo "<h1 class='tp3'>$titlepage3</h1><br>";
-    echo "<h3 class='tp4'>$titlepage4 $titlepage5</h3>";
-    // echo "<h3 class='tp4'>$titlepage4</h3>";
-    // echo "<h3 class='tp5'>$titlepage5</h3><br>";
-
-    echo "<h3 class='tp6'>$titlepage6<br></h3>";
-    // echo "<h3 class='tp6'>$titlepage6</h3>";
-    echo "<h3 class='tp7 imp-text-row'>$titlepage7</h3>";
-    echo "<h3 class='tp8 imp-text-row'>$titlepage8</h3>";
-    echo "<h3 class='tp9 imp-text-row'>$titlepage9</h3><br>";
     echo "<p class='text-author'>$byline</p>";
-    echo "<p class='license'>$imp1 $imp2 $imp3 $imp4</p>";
-    //echo "<p class='license'>$imp2</p>";
-    //echo $imp3;
-    // echo "<h3>Authors: $author1, &nbsp $author2</h3></br>";
-    // echo "<h3>Author: $author2</h3></br>";
+    echo "</div>";//close byline
 
-    // The following two statements can show all of the data but is not formatted
-    // echo $xml->asXML();
-    // var_dump($xml->asXML());
-}
+    echo "<div class='hrimp'>";
+    echo "<hr/>";
+    echo "</div>";
+
+    echo "<div class='imprimatur'>";
+    $imp1 = $xml->text->front->titlePage->imprimatur->seg[0];
+    $imp2 = $xml->text->front->titlePage->imprimatur->seg[1] .
+            $xml->text->front->titlePage->imprimatur->seg[2] .
+            $xml->text->front->titlePage->imprimatur->seg[3];
+
+    echo "<p class='license'>$imp1</p>";
+    echo "<p class='licdate'>$imp2</p>";
+    echo "</div>";
+
+    echo "<div class='hrimp'>";
+    echo "<hr/>";
+    echo "</div>";
+
+    echo "<div class='docimprint'>";
+    $placename = $xml->text->front->titlePage->docImprint->placeName->hi;
+    echo "<p class='doc-plname'>$placename</p>";
+
+    $di_line_ct = 1;
+    foreach($xml->text->front->titlePage->docImprint->line as $diline)
+    {
+        $di_line = $diline;
+        foreach($diline->seg as $diseg)
+        {
+            $di_line .= $diseg;
+            if ($diseg->persName->hi)
+            {
+                $di_line .= $diseg->persName->hi;
+            }
+            if ($diseg->placeName->hi)
+            {
+                $di_line .= $diseg->placeName->hi;
+            }
+            if ($diseg->date)
+            {
+                $di_line .= $diseg->date;
+            }
+        }
+        if ($di_line_ct == 1)
+        {
+            $di_first_line = $di_line;
+            $di_line = "";
+        }
+        else
+        {
+            $di_second_line = $di_line;
+        }
+        $di_line_ct++;
+    }
+    echo "<p class='di-text1'>$di_first_line</p>";
+    echo "<p class='di-text1'>$di_second_line</p>";
+
+    echo "</div>";//close docimprint
+
+    echo "<div class='hrcl'>";
+    echo "<hr/>";
+    echo "</div>";
+
+    echo "<div class='castList'>";
+    echo "<div class='castListHead'>";
+    $casthead = $xml->text->front->castList->head;
+    echo "<p class='clhead'>$casthead</p>";
+    echo "</div>";
+    $cg1cnt = 1;
+    $cg2cnt = 1;
+    $roleGrpCnt = 1;
+    $divCntItem = 1;
+    $segCnt = 1;
+    // $CG2 = $xml->text->front->castList->castGroup->castGroup->castItem;
+    // printf("%s has got %d children.\n", $CG2, $CG2->count());
+    foreach($xml->text->front->castList->castGroup as $castGroup1)
+    {
+        echo "<div class='outerCG$cg1cnt'>";
+
+        echo "<div class='outerCGhead$cg1cnt'>";
+        if ($castGroup1->head)
+        {
+            $cg1h = $castGroup1->head;
+            echo "<p class='cghead'>$cg1h</p>";
+        }
+        echo "</div>";
+
+        foreach($castGroup1->castGroup as $castGroup2)
+        {
+            echo "<div class='innerCG$cg2cnt'>";
+            foreach($castGroup2->castItem as $castItem)
+            {
+                if ($divCntItem == 1)
+                {
+                    echo "<div class='$cg1h$roleGrpCnt'>";
+
+                }
+                if ($castItem->role->name)
+                {
+                    $cItem = $castItem->role->name;
+                    $castItem .=  "<p class='ciname'>$cItem</p>";
+                    echo $castItem;
+                }
+                if ($castItem->roleDesc != null)
+                {
+                    $divCntItem = 0;
+                    echo "</div>";
+                }
+
+                if ($castItem->roleDesc->hi)
+                {
+                    $rDesc = $castItem->roleDesc->hi;
+                    $roleDesc = "<p class='rdesc1'>$rDesc</p>";
+                    if ($castItem->roleDesc->seg)
+                    {
+                        $rDesc = $castItem->roleDesc->seg;
+                        $roleDesc .= "<p class='rdesc2'>$rDesc</p>";
+                    }
+                    if ($castItem->roleDesc->seg[1]->hi)
+                    {
+                        $rDesc = $castItem->roleDesc->seg[1]->hi;
+                        $roleDesc .= "<p class='rdesc3'>$rDesc</p>";
+                    }
+                    $roleDesc = "<div class='roleDesc$roleGrpCnt'>$roleDesc</div><br>";
+                    echo $roleDesc;
+                }
+                if ($castItem->seg)
+                {
+                    foreach($castItem->seg as $ciSeg)
+                    {
+                        // echo $ciSeg;
+                        // $ciSeg = $castItem->seg[$segCnt];
+                        $castSeg .= "<p class='seg$segCnt'>$ciSeg</p>";
+                        $segCnt++;
+                    }
+                    $castSeg = "<div class='segDiv$roleGrpCnt'>$castSeg</div><br>";
+                    echo $castSeg;
+                }
+                $divCntItem++;
+                $roleGrpCnt++;
+            }
+            $cg2cnt++;
+            echo "</div>";
+        }
+        $cg1cnt++;
+        echo "</div>";
+    }
+    echo "</div>";
+
+    echo "<div class='hrset'>";
+    echo "<hr/>";
+    echo "</div>";
+
+    echo "<div class='set'>";
+
+    $set1 = $xml->text->front->set->seg[0];
+    $set2 = $xml->text->front->set->hi;
+    $set3 = $xml->text->front->set->seg[1];
+
+    $set = "<p class='set1'>$set1</p>";
+    $set .= "<p class='set2'>$set2</p>";
+    $set .= "<p class='rdesc1'>$set3</p>";
+
+    echo $set;
+
+
+    echo "</div>";//close set
+
+    echo "<div class='hrsalute'>";
+    echo "<hr/>";
+    echo "</div>";
+
+    echo "<div class='salute'>";
+
+    $saltitle = $xml->text->front->salute->title->p;
+
+    $saltitle = "<p class='set1'>$saltitle</p>";
+
+    echo $saltitle;
+
+    foreach ($xml->text->front->salute->p as $salute)
+    {
+        if ($salute->persName)
+        {
+            $salpersName = $salute->persName;
+            $salpersName = "<p class='salpersName'>$salpersName</p>";
+        }
+        foreach ($salute->seg as $salSeg)
+        {
+            $salText .= $salSeg;
+            if ($salSeg->hi)
+            {
+                $salText .= "<i>$salSeg->hi</i>";
+            }
+        }
+    }
+    echo "<div class='spName'>";
+    echo $salpersName;
+    echo "</div>";//close spName
+
+    echo "<div class='salTextDiv'>";
+    echo "<p class='salText'>";
+    echo $salText;
+    echo "</p>";
+    echo "</div>";//close salTextDiv
+
+    echo "</div>";//close salute
+
+
+    //
+    //Prolog goes HERE
+    //
+    // echo "<div class='hrpbody'>";
+    // echo "<hr/>";
+    // echo "</div>";
+    $stagecnt = 0;
+    foreach ($xml->text->body as $playBody)
+    {
+        foreach ($playBody->div as $pBodyDiv)
+        {
+            foreach ($pBodyDiv->head as $pBDHead)
+            {
+                foreach ($pBDHead->hi as $pBDHeadHi)
+                {
+                    echo "<div class='hract'>";
+                    echo "<hr/>";
+                    echo "</div>";
+
+                    $act = "<div class='act'><h1>$pBDHeadHi</h1></div>";
+                    echo $act;
+                    foreach ($pBDHead->stage as $pBDHeadStage)
+                    {
+                        $stagecnt++;
+                        echo "<div class='stagegrp$stagecnt'>";
+                        foreach ($pBDHeadStage->seg as $pBDHStageSeg)
+                        {
+                            foreach ($pBDHStageSeg->seg as $pBDHStageSeg2)
+                            {
+                                //$StageS2 = $pBDHStageSeg2;
+                                $StageSeg2 .= $pBDHStageSeg2;
+                                foreach ($pBDHStageSeg2->name as $pBDHStageName)
+                                {
+                                    // $stageNm = $pBDHStageName;
+
+                                    $StageName = "<i>$pBDHStageName</i>";
+                                    $StageSeg2 .= $StageName;
+                                }
+                                foreach ($pBDHStageSeg2->hi as $pBDHStageHi)
+                                {
+                                    // $stgHi = $pBDHStageHi;
+                                    $StageHi = "<i>$pBDHStageHi</i>";
+                                    $StageSeg2 .= $StageHi;
+                                }
+                            }
+                        }
+                        echo "<p class='stage'>$StageSeg2</p>";
+                        $StageSeg2 = "";
+                        echo "</div>";
+                    }
+                }
+
+            }
+            foreach ($pBodyDiv->sp as $pBSp)
+            {
+                foreach ($pBSp->stage as $pBSpStage)
+                {
+                    echo "<div class='innerstage'>";
+                    echo $pBSpStage;
+                    foreach ($pBSpStage->seg as $pBSpStageSeg)
+                    {
+                        echo $pBSpStageSeg;
+                        foreach ($pBSpStageSeg->seg as $pBSpStageSegSeg)
+                        {
+                            echo $pBSpStageSegSeg;
+                            foreach ($pBSpStageSegSeg->name as $pBSpStageSSName)
+                            {
+                                echo "<i>$pBSpStageSSName</i>";
+
+                            }
+                            foreach ($pBSpStageSegSeg->hi as $pBSpStageSSHi)
+                            {
+                                echo "<i>$pBSpStageSSHi</i>";
+
+                            }
+
+                        }
+                        foreach ($pBSpStageSeg->name as $pBSpStageSegName)
+                        {
+                            echo "<i>$pBSpStageSegName</i>";
+
+                        }
+
+                    }
+                    echo "</div>";
+                }
+
+                foreach ($pBSp->speaker as $pBSpSpeaker)
+                {
+                    echo "<div class='speaker'><i>$pBSpSpeaker->name</i></div>";
+                }
+
+                echo "<div class='spText'>";
+                foreach ($pBSp->l as $pBSpLine)
+                {
+                    if ($pBSpLine->seg->count() == 0 && $pBSpLine->stage->count() == 0)
+                    {
+                        echo "$pBSpLine<br>";
+                    }
+                    foreach ($pBSpLine->seg as $pBSpLineSeg)
+                    {
+                        $spLs = $pBSpLineSeg;
+                        $lineSeg .= $spLs;
+                        foreach ($pBSpLineSeg->name as $pBSpLineSegName)
+                        {
+                            $lineSeg .= $pBSpLineSegName;
+
+                        }
+                        foreach ($pBSpLineSeg->hi as $pBSpLineSegHi)
+                        {
+                            $lineSeg .= $pBSpLineSegHi;
+                        }
+                    }
+                    if ($lineSeg != "" && $lineSeg != null)
+                    {
+                        echo "$lineSeg<br>";
+                    }
+                    $lineSeg = "";
+                    foreach ($pBSpLine->stage as $pBSpLineStage)
+                    {
+                        $stageDir = $pBSpLineStage;
+                        foreach ($pBSpLineStage->seg as $pBSpLStageSeg)
+                        {
+                            $stageDir .= $pBSpLStageSeg;
+                        }
+                        echo "<div class='innerstage'>$stageDir</div>";
+                    }
+
+                }
+                echo "</div>";
+
+            }
+
+        }
+    }
+
+
+}//end of function parse_xml_file
 ?>
